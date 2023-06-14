0fc7266e2d4a54bede0a477ac3e7586f7a43d642
Luis Leguizamón <lleguizamonl@gmail.com>
2023-06-14 10:48:50 -0400

    Init: Add component for a basic login

---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const email = ref('');
+const password = ref('');
+
+function submit(event: Event){
+    event.preventDefault();
+    console.log(email.value);
+    console.log(password.value);
+    alert("send")
+}
+</script>
+
+<template>
+    <form @submit="submit">
+        <div>
+            <label for="email">Email</label>
+            <br>
+            <input v-model="email" id="email" type="email" required>
+        </div>
+        <br>
+        <div>
+            <label for="password">Password</label>
+            <br>
+            <input v-model="password" id="password" type="password" required>
+        </div>
+        <br>
+        <div>
+            <button type="submit">Sign in</button>
+        </div>
+    </form>
+</template>
