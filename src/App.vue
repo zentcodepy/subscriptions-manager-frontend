6b90f2e8bbd223dcba74564338e7057b36e7006b
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-08 16:08:29 -0400

    Style: Change UI of login and use tailwindui style

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,6 @@ import Login from './components/Login.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <Login/>
 </template>
 
