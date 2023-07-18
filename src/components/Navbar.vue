b79eeca411118c42bae0f7e502019030dd30bbd4
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-18 17:58:48 -0400

    Adjust: Route to dashboard

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -34,7 +34,7 @@
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                            <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
                                 aria-current="page">Dashboard</a>
                             <a href="#"
                                 class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Customers</a>
@@ -48,7 +48,7 @@
         <div class="sm:hidden" id="mobile-menu">
             <div class="space-y-1 px-2 pb-3 pt-2">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+                <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
                     aria-current="page">Dashboard
                 </a>
                 <a href="#"
--- src/router.ts
+++ src/router.ts
@@ -10,7 +10,7 @@ export default createRouter({
       component: Login,
     },
     {
-        path: '/dashboard',
+        path: '/',
         component: Dashboard,
     },
   ],
