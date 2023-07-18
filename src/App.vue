c0f6d201e34e114c2dde10148a2f0b3d688718db
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-18 12:15:30 -0400

    New: Add Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <script setup lang="ts">
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <router-view></router-view>
+  <Navbar></Navbar>
+  <div class="m-10">
+    <router-view></router-view>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
