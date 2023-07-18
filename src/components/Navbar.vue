f3be24556a2d6c60f01b423a3e1c6f4c63ae1f1a
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-18 18:02:46 -0400

    Style: Replace gray color for indigo in navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <script setup></script>
 <template>
-    <nav class="bg-gray-800">
+    <nav class="bg-indigo-600">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="relative flex h-16 items-center justify-between">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
                     <button type="button"
-                        class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                        class="inline-flex items-center justify-center rounded-md p-2 text-indigo-400 hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                         aria-controls="mobile-menu" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <!--
@@ -33,11 +33,11 @@
                 <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
-                            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                            <!-- Current: "bg-indigo-900 text-white", Default: "text-indigo-300 hover:bg-indigo-700 hover:text-white" -->
+                            <a href="/" class="bg-indigo-900 text-white rounded-md px-3 py-2 text-sm font-medium"
                                 aria-current="page">Dashboard</a>
                             <a href="#"
-                                class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Customers</a>
+                                class="text-indigo-300 hover:bg-indigo-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Customers</a>
                         </div>
                     </div>
                 </div>
@@ -47,12 +47,12 @@
         <!-- Mobile menu, show/hide based on menu state. -->
         <div class="sm:hidden" id="mobile-menu">
             <div class="space-y-1 px-2 pb-3 pt-2">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+                <!-- Current: "bg-indigo-900 text-white", Default: "text-indigo-300 hover:bg-indigo-700 hover:text-white" -->
+                <a href="/" class="bg-indigo-900 text-white block rounded-md px-3 py-2 text-base font-medium"
                     aria-current="page">Dashboard
                 </a>
                 <a href="#"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Customers
+                    class="text-indigo-300 hover:bg-indigo-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Customers
                 </a>
             </div>
         </div>
