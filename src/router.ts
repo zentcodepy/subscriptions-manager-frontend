26453dced1d6cf2750d7cc2c38b360c46ed35bda
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-18 15:48:55 -0400

    Bug: Remove routes of previously deleted component

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from './components/Dashboard.vue';
-import Home from './components/Home.vue';
 import Login from './components/Login.vue';
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
-    {
-      path: '/',
-      component: Home,
-    },
     {
       path: '/login',
       component: Login,
