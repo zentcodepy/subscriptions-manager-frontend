180dabb78317d65a8e747722805db5afbfd12779
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-25 13:23:35 -0400

    Refactor: Add TextInput component and use it for business name filter

--- src/pages/Customers/Partials/CustomerFilters.vue
+++ src/pages/Customers/Partials/CustomerFilters.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import TextInput from '../../../components/common/TextInput.vue';
 
 const emit = defineEmits(['input-emitted']);
 const businessName = ref('');
@@ -10,14 +11,10 @@ function handleInput() {
 </script>
 <template>
     <div class="grid grid-cols-4 gap-4">
-        <div class="relative mt-2 rounded-md shadow-sm">
-            <input 
-                type="text"
-                id="business_name"
-                v-model="businessName"
-                @input="handleInput()"
-                class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="Business Name">
-        </div>
+        <TextInput
+            placeholder="Business Name"
+            v-model="businessName"
+            @input="handleInput()">
+        </TextInput>
     </div>
 </template>
