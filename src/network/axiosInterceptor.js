c3ff611c1774895542279ddc52db413d5eff8f38
Luis Leguizamón <lleguizamonl@gmail.com>
2023-08-10 17:32:25 -0400

    New: Show progress bar for axios requests

--- src/network/axiosInterceptor.js
+++ src/network/axiosInterceptor.js
@@ -3,11 +3,32 @@ import { useRouter } from "vue-router";
 
 export function interceptResponse() {
   const router = useRouter();
-  axios.interceptors.response.use(
+
+  // Intercept request
+  axios.interceptors.request.use(
     function (config) {
+      // Show Loading Progress Bar
+      NProgress.start();
+
       return config;
     },
     function (error) {
+      console.log(error)
+    }
+  );
+
+  // Intercept response
+  axios.interceptors.response.use(
+    function (config) {
+      // Hide Loading Progress Bar
+      NProgress.done();
+
+      return config;
+    },
+    function (error) {
+      // Hide Loading Progress Bar
+      NProgress.done();
+
       const statusCode = error.response.status;
 
       switch (statusCode) {
