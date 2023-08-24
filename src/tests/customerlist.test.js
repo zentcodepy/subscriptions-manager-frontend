3ac0854f719dc900267ca5941af2b002da82f57e
Luis Leguizamón <lleguizamonl@gmail.com>
2023-08-24 10:55:26 -0400

    New: Test request to fetch customers with filters inside customer service

--- src/tests/customerlist.test.js
+++ src/tests/customerlist.test.js
@@ -26,4 +26,26 @@ describe("Customer Service", () => {
 
     expect(customers.data).toStrictEqual(customersMock);
   });
+
+  test("makes a GET request to fetch customers with filters", async () => {
+    const customersMock = [
+      { id: 1, business_name: "Company 1", document_number: "111000-1" },
+      { id: 2, business_name: "Company 2", document_number: "222000-1" },
+    ];
+
+    axios.get.mockResolvedValue({
+      data: customersMock,
+    });
+
+    const customers = await getCustomers("Company", 1);
+
+    expect(axios.get).toHaveBeenCalledWith("/base-url/api/customers", {
+      params: {
+        search: "Company",
+        page: 1,
+      },
+    });
+
+    expect(customers.data).toStrictEqual(customersMock);
+  });
 });
