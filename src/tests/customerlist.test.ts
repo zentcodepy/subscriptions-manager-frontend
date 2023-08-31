1175831753bb7416d5c117e2b4eabe05bb4761ac
Luis Leguizamón <lleguizamonl@gmail.com>
2023-08-31 12:09:06 -0400

    New: Test that customer list mounts properly

---
+++ src/tests/customerlist.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, it, test, vi } from "vitest";
+import { mount, RouterLinkStub } from "@vue/test-utils";
+import { Customer } from "../types/Customer.js";
+import axios from "axios";
+import List from "../pages/Customers/List.vue";
+
+vi.mock("axios");
+
+function getCustomerMock() {
+    return [
+        { id: 1, business_name: "Company 1", document_number: "111000-1" },
+        { id: 2, business_name: "Company 2", document_number: "222000-1" },
+    ];
+};
+
+describe("Customer List", () => {
+    test("Mounts properly", async () => {
+        const customersMock: Customer[] = getCustomerMock();
+
+        (axios as any).get.mockResolvedValue({
+            data: customersMock,
+        });
+
+        const wrapper = mount(List, {
+            global: {
+                stubs: {
+                    RouterLink: RouterLinkStub,
+                },
+            },
+        });
+
+        expect(wrapper).toBeTruthy();
+    });
+});
