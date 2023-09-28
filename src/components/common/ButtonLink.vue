a4874d34e6e930813ba1a40aad5af29f1a56b17c
Luis Leguizamón <lleguizamonl@gmail.com>
2023-09-28 17:29:31 -0400

    New: ButtonLink component

---
+++ src/components/common/ButtonLink.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    defineProps({
+        title: {
+            type: String,
+            default: ''
+        },
+        urlRoute: {
+            type: String,
+            default: '#'
+        }
+    });
+</script>
+<template>
+    <router-link 
+        :to=urlRoute
+        class="bg-indigo-600 text-white hover:bg-indigo-700 rounded-md px-3 py-2 text-sm font-medium"
+    >
+        {{ title }}
+    </router-link>
+</template>
