9f26a301fa139280461d57dcd95033f2f7b47edf
Luis Leguizamón <lleguizamonl@gmail.com>
2023-10-12 19:35:17 -0300

    New: Implement a Temporary Test for the Customer Edit Form

---
+++ src/tests/editcustomerform.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test, vi } from "vitest";
+import { shallowMount } from "@vue/test-utils";
+import CustomerForm from "../pages/Customers/CustomerForm.vue";
+
+// vi.mock('vue-router', () => ({
+//     useRoute: () => ({
+//         params: { customerId: 1 },
+//     }),
+// }));
+
+describe("Edit customer form", () => {
+    test("Update CustomerForm with valid data to the API", () => {
+        // shallowMount(CustomerForm, { global: { stubs: ['router-view'] } })
+    });
+})
