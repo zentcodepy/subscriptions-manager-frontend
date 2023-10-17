5119fd617eeb1bcd36ba188eb455085ce73fb3c6
Luis Leguizamón <lleguizamonl@gmail.com>
2023-10-17 10:16:57 -0300

    Improvement: Test edit customer form

--- src/tests/customers/editcustomerform.test.ts
+++ src/tests/customers/editcustomerform.test.ts
@@ -1,15 +1,45 @@
 import { describe, expect, test, vi } from "vitest";
-import { shallowMount } from "@vue/test-utils";
+import { flushPromises, mount } from "@vue/test-utils";
 import CustomerForm from "../../pages/Customers/CustomerForm.vue";
+import axios from "axios";
 
-// vi.mock('vue-router', () => ({
-//     useRoute: () => ({
-//         params: { customerId: 1 },
-//     }),
-// }));
+vi.mock("axios");
+
+(axios as any).get.mockResolvedValue({
+    status: 200,
+    data: {
+        data: {
+            id: 1,
+            business_name: "Company 1",
+        }
+    }
+        
+});
+
+(axios as any).put.mockResolvedValue({
+    status: 200,
+});
+
+(axios as any).post.mockResolvedValue({
+    status: 201,
+});
+
+vi.mock("vue-router", async () => {
+    const actual: any = await vi.importActual("vue-router")
+    return {
+        ...actual,
+        useRoute: () => ({
+            params: { customerId: 1 },
+        })
+    }
+});
 
 describe("Edit customer form", () => {
-    test("Update CustomerForm with valid data to the API", () => {
-        // shallowMount(CustomerForm, { global: { stubs: ['router-view'] } })
+    test("Update CustomerForm with valid data to the API", async () => {
+        const wrapper = mount(CustomerForm, { global: { stubs: ['router-view'] } })
+        await flushPromises();
+
+        const inputBusinessName = wrapper.find('input[id="business_name"]').element as HTMLInputElement;
+        expect(inputBusinessName.value).toBe('Company 1');
     });
 })
