e52f0ed3a7825ab93cc4c8d5f2a77af8e1a75003
Luis Leguizamón <lleguizamonl@gmail.com>
2023-10-31 17:43:28 -0300

    Adjust: Modify the subscription form's style to create a centered design

--- src/pages/Subscriptions/SubscriptionCreateForm.vue
+++ src/pages/Subscriptions/SubscriptionCreateForm.vue
@@ -65,120 +65,119 @@ function submit() {
 </script>
 <template>
     <AppLayout>
-        <form class="m-5" @submit.prevent="submit">
-            <div class="space-y-12">
+        <div class="mx-auto max-w-2xl text-center pt-5">
+            <h2 class="text-2xl font-semibold leading-7 text-gray-900">{{ formTitle }}</h2>
+        </div>
 
-                <div class="border-b border-gray-900/10 pb-12">
-                    <h2 class="text-base font-semibold leading-7 text-gray-900">{{ formTitle }}</h2>
+        <form class="mx-auto max-w-xl sm:mt-10" @submit.prevent="submit">
+            <div>
+                <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                    <!-- Date from -->
+                    <div>
+                        <TextInputWithLabel inputId="date_from" v-model="form.date_from" label="Date from"
+                            inputType="date" />
+                    </div>
+                    <!-- Duration in months -->
+                    <div>
+                        <label
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                            for="duration_in_months"
+                        >
+                            Duration in months
+                        </label>
+                        <input
+                            class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                            type="number"
+                            id="duration_in_months"
+                            v-model="form.duration_in_months"
+                            min=0
+                        >
+                    </div>
+                </div>
 
-                    <div class="mt-10 grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <!-- Date from -->
-                        <div class="sm:col-span-2">
-                            <TextInputWithLabel inputId="date_from" v-model="form.date_from" label="Date from"
-                                inputType="date" />
+                <div class="mt-10 grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                    <!-- Service -->
+                    <div class="sm:col-span-2">
+                        <label class="block text-sm font-medium leading-6 text-gray-900">
+                            Service
+                        </label>
+                        <VueMultiselect 
+                            v-model="form.service"
+                            label="name"
+                            placeholder="Select"
+                            track-by="id"
+                            :options="servicesOptions">
+                        </VueMultiselect>
+                    </div>
+                </div>
+
+                <div class="mt-10 grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                    <!-- Grace period in days -->
+                    <div class="sm:col-span-2">
+                        <label
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                            for="grace_period_in_days"
+                        >
+                            Grace Period In Days
+                        </label>
+                        <input
+                            class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                            type="number"
+                            id="grace_period_in_days"
+                            v-model="form.grace_period_in_days"
+                            min=0
+                        >
+                    </div>
+                </div>
+
+                <div class="mt-10 grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                    <!-- Total amount -->
+                    <div class="sm:col-span-2">
+                        <TextInputWithLabel inputId="total_amount" v-model="form.total_amount" label="Total amount"
+                            inputType="number" />
+                    </div>
+                </div>
+
+                <div class="mt-10 grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                    <!-- Payment Service Type -->
+                    <div class="sm:col-span-2">
+                        <InputLabel text="Payment Service Type" />
+                        <div class="mt-2">
+                            <InputRadio
+                                input-id="manual"
+                                input-name="payment_service_type"
+                                label="Manual"
+                                value="manual"
+                                v-model="form.payment_service_type"
+                            />
                         </div>
-                        <!-- Duration in months -->
-                        <div class="sm:col-span-2">
-                            <label
-                                class="block text-sm font-medium leading-6 text-gray-900"
-                                for="duration_in_months"
-                            >
-                                Duration in months
-                            </label>
-                            <input
-                                class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
-                                type="number"
-                                id="duration_in_months"
-                                v-model="form.duration_in_months"
-                                min=0
-                            >
+                        <div class="mt-2">
+                            <InputRadio
+                                input-id="metrepay"
+                                input-name="payment_service_type"
+                                label="Metrepay"
+                                value="metrepay"
+                                v-model="form.payment_service_type"
+                            />
                         </div>
                     </div>
+                </div>
 
-                    <div class="mt-10 grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <!-- Service -->
-                        <div class="sm:col-span-2">
-                            <label class="block text-sm font-medium leading-6 text-gray-900">
-                                Service
-                            </label>
-                            <VueMultiselect 
-                                v-model="form.service"
-                                label="name"
-                                placeholder="Select"
-                                track-by="id"
-                                :options="servicesOptions">
-                            </VueMultiselect>
-                        </div>
-                    </div>
-
-                    <div class="mt-10 grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <!-- Grace period in days -->
-                        <div class="sm:col-span-2">
-                            <label
-                                class="block text-sm font-medium leading-6 text-gray-900"
-                                for="grace_period_in_days"
-                            >
-                                Grace Period In Days
-                            </label>
-                            <input
-                                class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
-                                type="number"
-                                id="grace_period_in_days"
-                                v-model="form.grace_period_in_days"
-                                min=0
-                            >
-                        </div>
-                    </div>
-
-                    <div class="mt-10 grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <!-- Total amount -->
-                        <div class="sm:col-span-2">
-                            <TextInputWithLabel inputId="total_amount" v-model="form.total_amount" label="Total amount"
-                                inputType="number" />
-                        </div>
-                    </div>
-
-                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <!-- Payment Service Type -->
-                        <div class="sm:col-span-2">
-                            <InputLabel text="Payment Service Type" />
-                            <div class="mt-2">
-                                <InputRadio
-                                    input-id="manual"
-                                    input-name="payment_service_type"
-                                    label="Manual"
-                                    value="manual"
-                                    v-model="form.payment_service_type"
-                                />
-                            </div>
-                            <div class="mt-2">
-                                <InputRadio
-                                    input-id="metrepay"
-                                    input-name="payment_service_type"
-                                    label="Metrepay"
-                                    value="metrepay"
-                                    v-model="form.payment_service_type"
-                                />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="mt-10 grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <!-- Automatic notification enabled -->
-                        <div class="sm:col-span-2">
-                            <input 
-                                type="checkbox"
-                                id="automatic_notification_enabled"
-                                name="automatic_notification_enabled"
-                                v-model="form.automatic_notification_enabled"
-                            >
-                            <label for="automatic_notification_enabled"> Automatic notification enabled</label>
-                        </div>
+                <div class="mt-10 grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                    <!-- Automatic notification enabled -->
+                    <div class="sm:col-span-2">
+                        <InputLabel for="automatic_notification_enabled" text="Automatic notification enabled"></InputLabel>
+                        <input 
+                            type="checkbox"
+                            id="automatic_notification_enabled"
+                            name="automatic_notification_enabled"
+                            v-model="form.automatic_notification_enabled"
+                        >
                     </div>
                 </div>
             </div>
 
-            <div class="mt-6 flex items-center justify-start gap-x-6">
+            <div class="my-6 flex items-center justify-start gap-x-6">
                 <PrimaryButton title="Save"></PrimaryButton>
             </div>
         </form>
