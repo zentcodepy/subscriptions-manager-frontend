72adec717d20aeaa88419d49e523522a5e12c2d0
sebapastore <sebastianerp@gmail.com>
2023-11-07 12:08:55 -0300

    Create service form

--- src/pages/Services/ServiceCreateForm.vue
+++ src/pages/Services/ServiceCreateForm.vue
@@ -6,6 +6,7 @@ import router from '../../router';
 import { createService } from '../../services/ServiceService';
 import TextInputWithLabel from '../../components/common/TextInputWithLabel.vue';
 import PrimaryButton from '../../components/common/PrimaryButton.vue';
+import CustomerSearchInput from '../../components/customers/CustomerSearchInput.vue';
 
 const form = ref<CreateServiceData>({
     name: '',
@@ -36,27 +37,27 @@ function submit() {
 </script>
 <template>
     <AppLayout>
-        <form class="m-5" @submit.prevent="submit">
+        <div class="mx-auto max-w-2xl text-center pt-5">
+            <h2 class="text-2xl font-semibold leading-7 text-gray-900">{{ formTitle }}</h2>
+        </div>
+
+        <form class="mx-auto max-w-xl sm:mt-10 px-8" @submit.prevent="submit">
             <div class="space-y-12">
 
                 <div class="border-b border-gray-900/10 pb-12">
                     <h2 class="text-base font-semibold leading-7 text-gray-900">{{ formTitle }}</h2>
 
-                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div class="sm:col-span-2">
-                            <TextInputWithLabel
-                                inputId="customer_id"
-                                v-model="form.customer_id"
-                                label="Customer Id"
-                            />
-                        </div>
-
+                    <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                         <div class="sm:col-span-2">
                             <TextInputWithLabel
                                 inputId="name"
                                 v-model="form.name"
                                 label="Service Name"
                             />
+                        </div>
+
+                        <div class="sm:col-span-2">
+                          <customer-search-input @update:customer-id="customerId => form.customer_id = customerId"></customer-search-input>
                         </div>
                     </div>
 
